846f7f4b846549f417e89803404b045dac73f4b4
Nyando Onongwene <nyandoonotex@gmail.com>
2024-01-31 20:31:12 +0100

    Initial commit

---
+++ src/main/java/org/example/Welcome1.java
@@ -0,0 +1,17 @@
+package org.example;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+//    }
+//}
+
+
+// Text-printing program.
+
+         public class Welcome1 {
+// main method begins execution of Java application
+         public static void main(String[] args) {
+        System.out.println("Welcome to Java Programming!");
+        } // end method main
+ } // end class Welcome1
