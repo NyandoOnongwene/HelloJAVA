0f07295aa293110d89778bcdd159d1ee5159ac56
Nyando Onongwene <nyandoonotex@gmail.com>
2024-01-31 20:45:16 +0100

    printing text on new lines

---
+++ src/main/java/org/example/Welcome2.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Welcome2 {
+
+            // Printing multiple lines of text with a single statement.
+
+// main method begins execution of Java application
+        public static void main(String[] args) {
+            System.out.println("Welcome\nto\nJava\nProgramming!");
+             } // end method main
+} // end class Welcome3
+
